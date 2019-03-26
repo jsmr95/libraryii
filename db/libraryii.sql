fa17285875d18e414f9f661fa2bae48b368a3cc1
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-03-26 23:21:25 +0100

    Tabla usuarios y usuarios_id

--- db/libraryii.sql
+++ db/libraryii.sql
@@ -1,4 +1,27 @@
 ------------------------------
 -- Archivo de base de datos --
 ------------------------------
-/*Primeras tablas*/
+
+/* Tabla usuarios_id, este id de usuario no se eliminará nunca*/
+DROP TABLE IF EXISTS usuarios_id CASCADE;
+
+CREATE TABLE usuarios_id
+(
+    id      BIGSERIAL PRIMARY KEY
+);
+
+/* Tabla de usuarios en sí, la que contiene todos los datos de un
+    usuario registrado */
+DROP TABLE IF EXISTS usuarios
+(
+      id        bigint          PRIMARY KEY REFERENCES usuarios_id (id)
+    , login     varchar(255)    NOT NULL UNIQUE
+    , email     varchar(255)    NOT NULL UNIQUE
+    , nombre    varchar(255)    NOT NULL
+    , apellido  varchar(255)    NOT NULL
+    , biografia varchar(255)
+    , url_avatar varchar(255)
+    , pass      varchar(255)    NOT NULL
+    , auth_key  varchar(255)    NOT NULL
+    , created_at timestamp(0)   NOT NULL DEFAULT localtimestamp
+);
