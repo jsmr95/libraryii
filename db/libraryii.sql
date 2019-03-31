48c9d36d78ba65a560ee107dbb6973119c080ad4
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-04-01 00:44:51 +0200

    Demas tablas - Falta valores, ampliar estas tablas y refactorizar

--- db/libraryii.sql
+++ db/libraryii.sql
@@ -12,11 +12,11 @@ CREATE TABLE usuarios_id
 
 /* Tabla de usuarios en sí, la que contiene todos los datos de un
     usuario registrado */
-DROP TABLE IF EXISTS usuarios
+DROP TABLE IF EXISTS usuarios CASCADE;
 
 CREATE TABLE usuarios
 (
-      id        bigint          PRIMARY KEY REFERENCES usuarios_id (id)
+      id        bigint          PRIMARY KEY REFERENCES usuarios_id (id) ON DELETE CASCADE ON UPDATE CASCADE
     , login     varchar(255)    NOT NULL UNIQUE
     , email     varchar(255)    NOT NULL UNIQUE
     , nombre    varchar(255)    NOT NULL
@@ -29,6 +29,26 @@ CREATE TABLE usuarios
     , updated_at timestamp(0)
 );
 
+/* Tabla Generos */
+DROP TABLE IF EXISTS generos CASCADE;
+
+CREATE TABLE generos
+(
+      id        BIGSERIAL PRIMARY KEY
+    , genero    varchar(255) NOT NULL
+);
+
+/* Tabla autores */
+DROP TABLE IF EXISTS autores CASCADE;
+
+CREATE TABLE autores
+(
+      id        BIGSERIAL PRIMARY KEY
+    , nombre    varchar(255) NOT NULL
+    , descripcion    varchar(255) NOT NULL
+    , anyo      numeric(4) NOT NULL
+);
+
 /* Tabla Libros */
 DROP TABLE IF EXISTS libros CASCADE;
 
@@ -37,9 +57,56 @@ CREATE TABLE libros
       id        BIGSERIAL PRIMARY KEY
     , titulo    varchar(255) NOT NULL
     , isbn      varchar(255) NOT NULL
+    , anyo      numeric(4) NOT NULL
     , sinopsis  varchar(255) NOT NULL
     , url_compra varchar(255) NOT NULL
-    , anyo      numeric(4) NOT NULL
-    , autor_id  varchar(255) PRIMARY KEY REFERENCES autor (id)
-    , genero_id varchar(255) PRIMARY KEY REFERENCES genero (id)
+    , autor_id  BIGINT REFERENCES autores (id) ON DELETE CASCADE ON UPDATE CASCADE
+    , genero_id BIGINT REFERENCES generos (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+/* Tabla comentarios */
+DROP TABLE IF EXISTS comentarios CASCADE;
+
+CREATE TABLE comentarios
+(
+      id        BIGSERIAL PRIMARY KEY
+    , usuario_id  BIGINT REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE
+    , libro_id  BIGINT REFERENCES libros (id) ON DELETE CASCADE ON UPDATE CASCADE
+    , comentario_id  BIGINT REFERENCES comentarios (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/* Tabla Posts */
+DROP TABLE IF EXISTS posts CASCADE;
+
+CREATE TABLE posts
+(
+      id        BIGSERIAL PRIMARY KEY
+    , contenido varchar(255) NOT NULL
+    , created_at timestamp(0)   NOT NULL DEFAULT localtimestamp
+    , usuario_id  BIGINT REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/* Tabla Libros favoritos */
+DROP TABLE IF EXISTS libros_favs CASCADE;
+
+CREATE TABLE libros_favs
+(
+      id        BIGSERIAL PRIMARY KEY
+    , usuario_id  BIGINT REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE
+    , libro_id  BIGINT REFERENCES libros (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/* Tabla Autores favoritos */
+DROP TABLE IF EXISTS autores_favs CASCADE;
+
+CREATE TABLE autores_favs
+(
+      id        BIGSERIAL PRIMARY KEY
+    , usuario_id  BIGINT REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE
+    , autor_id  BIGINT REFERENCES autores (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+------------------------
+-------- VALUES --------
+------------------------
