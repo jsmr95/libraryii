83d6e8f75cd052fc48d7756d40100de3d9b2bc53
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-04-02 22:11:24 +0200

    Inserto valores

--- db/libraryii.sql
+++ db/libraryii.sql
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS usuarios CASCADE;
 
 CREATE TABLE usuarios
 (
-      id            bigint          PRIMARY KEY REFERENCES usuarios_id (id)
+      usuario_id            bigint  PRIMARY KEY REFERENCES usuarios_id (id)
                                         ON DELETE CASCADE
                                         ON UPDATE CASCADE
     , login         varchar(255)    NOT NULL UNIQUE
@@ -48,7 +48,6 @@ CREATE TABLE autores
       id            BIGSERIAL PRIMARY KEY
     , nombre        varchar(255) NOT NULL
     , descripcion   varchar(255) NOT NULL
-    , anyo          numeric(4) NOT NULL
 );
 
 /* Tabla Libros */
@@ -76,7 +75,7 @@ DROP TABLE IF EXISTS comentarios CASCADE;
 CREATE TABLE comentarios
 (
       id                BIGSERIAL PRIMARY KEY
-    , usuario_id        BIGINT REFERENCES usuarios (id)
+    , usuario_id        BIGINT REFERENCES usuarios (usuario_id)
                             ON DELETE CASCADE
                             ON UPDATE CASCADE
     , libro_id          BIGINT REFERENCES libros (id)
@@ -95,7 +94,7 @@ CREATE TABLE posts
       id            BIGSERIAL PRIMARY KEY
     , contenido     varchar(255) NOT NULL
     , created_at    timestamp(0)   NOT NULL DEFAULT localtimestamp
-    , usuario_id    BIGINT REFERENCES usuarios (id)
+    , usuario_id    BIGINT REFERENCES usuarios (usuario_id)
                         ON DELETE CASCADE
                         ON UPDATE CASCADE
 );
@@ -106,7 +105,7 @@ DROP TABLE IF EXISTS libros_favs CASCADE;
 CREATE TABLE libros_favs
 (
       id            BIGSERIAL PRIMARY KEY
-    , usuario_id    BIGINT REFERENCES usuarios (id)
+    , usuario_id    BIGINT REFERENCES usuarios (usuario_id)
                         ON DELETE CASCADE
                         ON UPDATE CASCADE
     , libro_id      BIGINT REFERENCES libros (id)
@@ -120,10 +119,24 @@ DROP TABLE IF EXISTS autores_favs CASCADE;
 CREATE TABLE autores_favs
 (
       id            BIGSERIAL PRIMARY KEY
-    , usuario_id    BIGINT REFERENCES usuarios (id)
+    , usuario_id    BIGINT REFERENCES usuarios (usuario_id)
                         ON DELETE CASCADE
                         ON UPDATE CASCADE
     , autor_id      BIGINT REFERENCES autores (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE
+);
+
+/* Tabla Usuarios favoritos */
+DROP TABLE IF EXISTS users_favs CASCADE;
+
+CREATE TABLE users_favs
+(
+      id            BIGSERIAL PRIMARY KEY
+    , usuario_id    BIGINT REFERENCES usuarios (usuario_id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE
+    , usuario_fav      BIGINT REFERENCES usuarios (usuario_id)
                         ON DELETE CASCADE
                         ON UPDATE CASCADE
 );
@@ -139,47 +152,109 @@ VALUES (DEFAULT), (DEFAULT), (DEFAULT), (DEFAULT), (DEFAULT), (DEFAULT),
        (DEFAULT), (DEFAULT);
 
 -- Usuarios --
-INSERT INTO usuarios (id, login, email, nombre, apellido, biografia, url_avatar,
+INSERT INTO usuarios (usuario_id, login, email, nombre, apellido, biografia, url_avatar,
     pass, auth_key, created_at, updated_at)
-VALUES (1, 'jose', 'jose@jose.com', 'jose', 'gallego', 'Joven programador...',
-            '', crypt('jose123', gen_salt('bf',10)), '', , )
-    ,  (2, ...)
-    ,  (3, ...)
-    ,  (4, ...)
-    ,  (5, ...)
-    ,  (6, ...)
-    ,  (7, ...)
-    ,  (8, ...)
+VALUES (1, 'jose', 'jose@jose.com', 'jose', 'Gallego', 'Joven programador...',
+            null, crypt('jose123', gen_salt('bf',10)), '', DEFAULT, null)
+    ,  (2, 'pepe', 'pepe@pepe.com', 'pepe', 'Ruiz', 'Programador senior...',
+                null, crypt('pepe123', gen_salt('bf',10)), '', DEFAULT, null)
+    ,  (3, 'admin', 'admin@admin.com', 'admin', 'admin', 'Un administrador...',
+                null, crypt('admin123', gen_salt('bf',10)), '', DEFAULT, null)
+    ,  (4, 'carmen', 'carmen@carmen.com', 'carmen', 'gallego', 'Joven programador...',
+                null, crypt('carmen123', gen_salt('bf',10)), '', DEFAULT, null);
 
 -- Autores --
-INSERT INTO autores (id, nombre, descripcion, anyo)
+INSERT INTO autores (nombre, descripcion)
 VALUES ('Stephen King', 'Stephen Edwin King es un escritor estadounidense
-            conocido por sus novelas de terror. Los libros ...', 1947)
+            conocido por sus novelas de terror. Los libros ...')
     ,  ('Carlos Ruiz Zafón', 'Escritor con una temprana vocación, comenzó
-            con literatura juvenil con la novela "El principe de la niebla".', 1964)
+            con literatura juvenil con la novela "El principe de la niebla".')
     ,  ('Isabel Allende', 'Isabel Allende Llona es una reconocida escritora
-            y periodista chilena, conocida principalmente por sus novelas ...', 1942)
+            y periodista chilena, conocida principalmente por sus novelas ...')
     ,  ('Georg R. R. Martin', 'George Raymond Richard Martin conocido por sus
-            seguidores como GRRM, es un escritor y guionista estadounidense ...', 1948)
+            seguidores como GRRM, es un escritor y guionista estadounidense ...')
     ,  ('Arturo Pérez Reverte', 'Es un periodista y escritor español, miembro
-            de la Real Academia Española. Antiguo corresponsal de Radio...', 1951)
+            de la Real Academia Española. Antiguo corresponsal de Radio...')
     ,  ('Megan Maxwell', 'Megan Maxwell es una conocida escritora de literatura
-            romántica de origen español, por parte de madre y norteamericano,...', 1965);
+            romántica de origen español, por parte de madre y norteamericano,...')
+    ,  ('Antoine de Saint Exupéry', 'Antoine de Saint-Exupéry(Lyon-Marsella) fue
+            un escritor y aviador francés, autor de "El principito"');
 
 -- Generos --
 INSERT INTO generos ( genero)
-VALUES ('Artes', 'Aventuras', 'Biografías y Memorias', 'Ciencia-Ficción'
-        , 'Ciencias y Naturaleza', 'Clásicos', 'Erótica', 'Fantasía', 'Histórica y Bélica'
-        , 'Humor', 'Infantil', 'Informática y Tecnología', 'Literatura', 'Narrativa'
-        , 'Poesía', 'Romántica', 'Superhéroes', 'Teatro');
-
--- Generos --
-INSERT INTO generos ( genero)
-VALUES ('Artes', 'Aventuras', 'Biografías y Memorias', 'Ciencia-Ficción'
-        , 'Ciencias y Naturaleza', 'Clásicos', 'Erótica', 'Fantasía', 'Histórica y Bélica'
-        , 'Humor', 'Infantil', 'Informática y Tecnología', 'Literatura', 'Narrativa'
-        , 'Poesía', 'Romántica', 'Superhéroes', 'Teatro');
+VALUES ('Artes')
+        , ('Aventuras')
+        , ('Biografías y Memorias')
+        , ('Ciencia-Ficción')
+        , ('Ciencias y Naturaleza')
+        , ('Clásicos')
+        , ('Erótica')
+        , ('Fantasía')
+        , ('Histórica y Bélica')
+        , ('Humor')
+        , ('Infantil')
+        , ('Informática y Tecnología')
+        , ('Literatura')
+        , ('Narrativa')
+        , ('Poesía')
+        , ('Romántica')
+        , ('Superhéroes')
+        , ('Teatro')
+        , ('Novela')
+        , ('Juvenil no ficción');
 
 -- Libros --
 INSERT INTO libros ( titulo, isbn, anyo, sinopsis, url_compra, autor_id, genero_id)
-VALUES ();
+VALUES ('El principito', '9788498381498', 1943, 'El principito habita un pequeñísimo
+            asteroide, que comparte con una flor y empieza a experimentar la soledad.'
+            , 'https://www.amazon.es/gp/product/8498381495/ref=as_li_tf_tl?ie=UTF8&tag=entrel-21&linkCode=as2'
+            , 7, 13)
+    ,  ('El resplandor', '9789875668478', 1977, 'Tenía una deuda pendiente con Stephen King.
+            Crecí rodeado de sus libros;It, Cementerio de animales, ...'
+            , 'https://www.amazon.es/gp/product/9875668478/ref=as_li_tf_tl?ie=UTF8&tag=entrel-21&linkCode=as2'
+            , 1, 19)
+    ,  ('Marina', '9788423687268', 1999, 'Óscar Drai se marchó huyendo de sus recuerdos, pensando ingenuamente que,
+            si ponía suficiente distancia, las voces de su pasado se acallarían para siempre ...'
+            , 'https://www.amazon.es/gp/product/8423687260/ref=as_li_tf_tl?ie=UTF8&tag=entrel-21&linkCode=as2'
+            , 2, 20)
+    ,  ('La suma de los días', '9788401341915', 2007, 'Isabel Allende narra a su hija Paula
+            todo lo que ha sucedido con la familia desde el momento en que ella murió. El lector vive, junto con la autora...'
+            , 'https://www.amazon.es/gp/product/8401341914/ref=as_li_tf_tl?ie=UTF8&tag=entrel-21&linkCode=as2'
+            , 3, 3)
+    ,  ('Juego de tronos (canción de hielo y fuego #1)', '9788496208964', 1996, 'Primer libro de la saga CANCIÓN DE HIELO Y FUEGO.
+            «En un mundo donde las estaciones pueden durar decenios y donde las tierras del norte,
+            más allá del Muro, ocultan seres míticos y temibles,...'
+            , 'https://www.amazon.es/gp/product/8496208966/ref=as_li_tf_tl?ie=UTF8&tag=entrel-21&linkCode=as2'
+            , 4, 8)
+    ,  ('Las ranas también se enamoran', '9788492929467', 2011, 'Marta Rodríguez es una joven y divertida madre
+              soltera que conduce una Honda CBF 600. Trabaja en el taller de moda flamenca de Lola Herrera,...'
+            , 'https://www.amazon.es/gp/product/8492929464/ref=as_li_tf_tl?ie=UTF8&tag=entrel-21&linkCode=as2'
+            , 6, 13);
+
+-- Comentarios --
+INSERT INTO comentarios (usuario_id, libro_id, comentario_id)
+VALUES  (1,1,null)
+        ,  (1, 2, null)
+        ,  (2, 3, null)
+        ,  (3, 4, null);
+
+-- Libros_favs --
+INSERT INTO libros_favs (usuario_id, libro_id)
+VALUES  (1,1)
+        ,  (1, 2)
+        ,  (2, 2)
+        ,  (3, 2);
+
+-- Autores_favs --
+INSERT INTO autores_favs (usuario_id, autor_id)
+VALUES  (1,1)
+        ,  (1, 1)
+        ,  (2, 1)
+        ,  (3, 1);
+
+-- Usuarios_favs --
+INSERT INTO users_favs (usuario_id, usuario_fav)
+VALUES  (1,2)
+        ,  (1, 3)
+        ,  (2, 3)
+        ,  (3, 1);
