f8d866081890b00b9cca64014f114b85aaaf9288
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-04-12 22:30:22 +0200

    CRUD y modelo de Estados

---
+++ models/Estados.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "estados".
+ *
+ * @property int $id
+ * @property int $usuario_id
+ * @property string $estado
+ * @property string $created_at
+ *
+ * @property UsuariosId $usuario
+ */
+class Estados extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'estados';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['usuario_id'], 'default', 'value' => null],
+            [['usuario_id'], 'integer'],
+            [['estado'], 'required'],
+            [['created_at'], 'safe'],
+            [['estado'], 'string', 'max' => 255],
+            [['usuario_id'], 'exist', 'skipOnError' => true, 'targetClass' => UsuariosId::className(), 'targetAttribute' => ['usuario_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'usuario_id' => 'Usuario ID',
+            'estado' => 'Estado',
+            'created_at' => 'Created At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsuario()
+    {
+        return $this->hasOne(UsuariosId::className(), ['id' => 'usuario_id'])->inverseOf('estados');
+    }
+}
