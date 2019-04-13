f73075fc535f97092f42b03e916dd7e16f184b71
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-04-13 10:50:52 +0200

    Muestro el avatar en el estado de un usuario

--- views/estados/_estados.php
+++ views/estados/_estados.php
@@ -7,15 +7,35 @@ use yii\helpers\Html;
 
 ?>
 <style media="screen">
-
+.cuerpo_estado {
+    background-color: #e8edff;
+    position: relative;
+    padding: 5px 10px 5px 30px;
+    margin: 20px 0px 20px 0px;
+    border-radius: 6px;
+    -webkit-box-shadow: 7px 7px 45px -23px rgba(0,0,0,0.75);
+    -moz-box-shadow: 7px 7px 45px -23px rgba(0,0,0,0.75);
+    box-shadow: 7px 7px 45px -23px rgba(0,0,0,0.75);
+}
+.estado_texto {
+    padding-top: 30px;
+    padding-bottom: 50px;
+    font-family: cursive;
+}
 </style>
 <div class="row ">
-    <div class=" col-md-12">
-        <div class=" col-md-3">
+    <div class="cuerpo_estado col-md-12">
+        <div class=" col-md-2">
             <!-- Imagen AQUI -->
             <center>
                 <p>
-
+                    <?php
+                    if (empty($model->imagen)) {
+                        echo Html::img(Yii::getAlias('@uploads').'/userAutorDefecto.jpeg');
+                    } else {
+                        echo Html::img(Yii::getAlias('@uploads').'/'.$model->imagen);
+                    }
+                    ?>
                 </p>
             </center>
         </div>
@@ -38,7 +58,7 @@ use yii\helpers\Html;
                 </small>
             </h2>
         </span>
-            <p class="autor-texto">
+            <p class="estado_texto">
                 <?= $model->estado  ?>
             </p>
         </div>
