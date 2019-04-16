d37dee657caa9f5ba17838a515d92a20176a54ad
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-04-16 13:13:26 +0200

    Ver usuario

--- views/usuarios/view.php
+++ views/usuarios/view.php
@@ -6,19 +6,32 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Usuarios */
 
-$this->title = 'Ver Usuario';
+$this->title = $model->nombre . ' ' . $model->apellido;
 $this->params['breadcrumbs'][] = ['label' => 'Social', 'url' => ['estados/index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="usuarios-view">
+<style>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+img[src^="https://s3.eu-west-2.amazonaws.com/imagesjsmr95"] {
+    width: 190px !important;
+    height: 225px !important;
+    border-radius: 110px;
+}
+
+</style>
+<div class="container">
+
+    <center>
+        <h1><?= Html::encode($this->title) ?></h1>
+    </center>
 
     <?php
     if (!Yii::$app->user->isGuest){
         if (Yii::$app->user->identity->login === $model->login){
             ?>
+<div class="row">
+    <div class="col-md-offset-5 col-md-2">
     <p>
         <?= Html::a('Modificar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Eliminar cuenta', ['delete', 'id' => $model->id], [
@@ -29,20 +42,55 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ]) ?>
     </p>
+</div>
+</div>
 <?php } }  ?>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'login',
-            'email:email',
-            'nombre',
-            'apellido',
-            'biografia',
-            'url_avatar:url',
-            'created_at',
-            'updated_at',
-        ],
-    ]) ?>
+    <div class="row">
+        <div class="col-md-offset-5 col-md-2">
+            <br>
+            <?php
+            if (empty($model->imagen)) {
+                echo Html::img(Yii::getAlias('@uploads').'/userAutorDefecto.jpeg');
+            } else {
+                echo Html::img(Yii::getAlias('@uploads').'/'.$model->imagen);
+            }
+            ?>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-primary">
+              <div class="panel-heading">Información Cuenta</div>
+              <div class="panel-body">
+                  <p>Login: <?= $model->login ?></p>
+                  <p>Email: <?= $model->email ?></p>
+              </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-primary">
+              <div class="panel-heading">Información Personal</div>
+              <div class="panel-body">
+                  <p>Nombre: <?= $model->nombre ?></p>
+                  <p>Apellido: <?= $model->apellido ?></p>
+                  <p>Biografía: <?= $model->biografia ?></p>
+                  <p>Autenticado: <?php
+                   if (empty($model->auth_key)) {?>
+                       Verificado!
+                   <?php } else {?>
+                   NO verificado!
+                   <?php } ?>
+                   </p>
+                  <p>Creado: <?= $model->created_at ?></p>
+                  <p>Última modificación: <?= $model->updated_at ?></p>
+              </div>
+            </div>
+        </div>
+    </div>
 
 </div>
