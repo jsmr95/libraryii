86d73fab47b5e0b7fb775a375967913c7ccf6487
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-05-16 20:31:25 +0200

    Modifico la vista para mostrar el ultimo lanzamiento

--- views/site/_ultimoLanzamiento.php
+++ views/site/_ultimoLanzamiento.php
@@ -25,41 +25,32 @@ use yii\helpers\Html;
 }
 
 img.librosUsuarios {
-    width: 50px !important;
-    height: 70px !important;
+    width: 150px !important;
+    height: 200px !important;
     border-radius: 3px;
     margin-top: 20px !important;
 }
 </style>
 <div class="row libro">
     <!-- Fila de cada libro -->
-    <div class="libro-cuerpo col-md-10 col-md-offset-1">
-        <!-- Columna de 10 y separado 1 para cada libro-->
-        <div class="col-md-2">
-            <!-- Imagen AQUI -->
-            <center>
-                <p>
-                <?php
-                if (empty($model->imagen)) {
-                    echo Html::img(Yii::getAlias('@uploads').'/libroDefecto.png', ['class' => 'librosUsuarios']);
-                } else {
-                    echo Html::img(Yii::getAlias('@uploads').'/'.$model->imagen, ['class' => 'librosUsuarios']);
-                }
-                ?>
-                </p>
-            </center>
-        </div>
-    <div class="row">
-        <!-- Fila para cada titulo del libro-->
-        <div class=" col-md-8 col-md-offset-1">
-            <!-- Columna de 8 y separada 1 para el titulo-->
-            <center>
-                <h5>
-                    <?= Html::a($model->titulo, ['libros/view', 'id' => $model->id], ['class' => 'tituloLibro']) ?>
-                </h5>
-            </center>
-        </div>
-    </div>
-    </div>
-
+    <!-- Imagen AQUI -->
+    <center>
+        <p>
+        <?php
+        if (empty($model->imagen)) {
+            echo Html::img(Yii::getAlias('@uploads').'/libroDefecto.png', ['class' => 'librosUsuarios']);
+        } else {
+            echo Html::img(Yii::getAlias('@uploads').'/'.$model->imagen, ['class' => 'librosUsuarios']);
+        }
+        ?>
+        </p>
+    </center>
+</div>
+<div class="row" style="margin-top: -30px">
+    <!-- Fila para cada titulo del libro-->
+    <center>
+        <h5>
+            <?= Html::a($model->titulo, ['libros/view', 'id' => $model->id], ['class' => 'tituloLibro']) ?>
+        </h5>
+    </center>
 </div>
