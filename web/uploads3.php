ab3e2387b097e60fafa70664909f185523392cfc
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-05-18 19:25:01 +0200

    Ya me sube la imagen

--- web/uploads3.php
+++ web/uploads3.php
@@ -15,7 +15,7 @@
 // https://packagist.org/packages/aws/aws-sdk-php
 //
 // Run:$ composer require aws/aws-sdk-php
-require './vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use Aws\S3\Exception\S3Exception;
 use Aws\S3\S3Client;
@@ -25,19 +25,20 @@ $bucketName = 'imagesjsmr95';
 $IAM_KEY = 'AKIAJV2XC7DOVIQH4YNQ';
 $IAM_SECRET = '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';
 // Connect to AWS
+
 try {
     // You may need to change the region. It will say in the URL when the bucket is open
     // and on creation.
     $s3 = S3Client::factory(
         [
-            'credentials' => [
-                'key' => $IAM_KEY,
-                'secret' => $IAM_SECRET,
-            ],
-            'version' => 'latest',
-            'region' => 'us-east-2',
-        ]
-    );
+        'credentials' => [
+            'key' => $IAM_KEY,
+            'secret' => $IAM_SECRET,
+        ],
+        'version' => 'latest',
+        'region' => 'eu-west-2',
+    ]
+);
 } catch (Exception $e) {
     // We use a die, so if this fails. It stops here. Typically this is a REST call so this would
     // return a json object.
@@ -45,25 +46,26 @@ try {
 }
 
 // For this, I would generate a unqiue random string for the key name. But you can do whatever.
-$keyName = basename($_FILES['fileToUpload']['tmp_name']);
-$pathInS3 = 'https://s3.us-east-2.amazonaws.com/' . $bucketName . '/' . $keyName;
+$keyName = basename($_FILES['fileToUpload']['name']);
+$pathInS3 = 'https://s3.eu-west-2.amazonaws.com/' . $bucketName . '/' . $keyName;
 // Add it to S3
 try {
     // Uploaded:
     $file = $_FILES['fileToUpload']['tmp_name'];
     $s3->putObject(
         [
-            'Bucket' => $bucketName,
-            'Key' => $keyName,
-            'SourceFile' => $file,
-            'StorageClass' => 'REDUCED_REDUNDANCY',
-        ]
-    );
+        'Bucket' => $bucketName,
+        'Key' => $keyName,
+        'SourceFile' => $file,
+        'StorageClass' => 'REDUCED_REDUNDANCY',
+    ]
+);
 } catch (S3Exception $e) {
     die('Error:' . $e->getMessage());
 } catch (Exception $e) {
     die('Error:' . $e->getMessage());
 }
-echo 'Done';
+
+
 // Now that you have it working, I recommend adding some checks on the files.
 // Example: Max size, allowed file types, etc.
