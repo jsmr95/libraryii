f5db2054768fdcd83268af9ee782ef4b00919cbd
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-05-27 20:04:25 +0200

    Mejoro vista de logueo

--- views/site/login.php
+++ views/site/login.php
@@ -1,3 +1,14 @@
+<style media="screen">
+
+    .field-loginform-username, .field-loginform-password, .field-loginform-rememberme{
+        margin-left: 36% !important;
+    }
+    #loginform-username:focus, #loginform-password:focus{
+        transition: 0.3s;
+        width: 230px;
+        border-radius: 20px;
+    }
+</style>
 <?php
 
 /* @var $this yii\web\View */
@@ -7,14 +18,17 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+$this->title = 'Logueo';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
+    <center>
+        <h1><?= Html::encode($this->title) ?></h1>
+        <p>Use su usuario o email y contraseña para loguearse:</p>
+    </center>
     <br>
+    <center>
+
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'layout' => 'horizontal',
@@ -22,20 +36,23 @@ $this->params['breadcrumbs'][] = $this->title;
             'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
             'labelOptions' => ['class' => 'col-lg-1 control-label'],
         ],
+        'options' => [
+            'class' => 'box',
+        ]
     ]); ?>
 
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'placeholder' => 'Usuario/Email'])->label(false) ?>
 
-        <?= $form->field($model, 'password')->passwordInput() ?>
+        <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Contraseña'])->label(false) ?>
 
         <?= $form->field($model, 'rememberMe')->checkbox([
             'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
+        ])->label('Recuerdame') ?>
 
     <div class="form-group">
         <div class="col-lg-offset-1 col-lg-11">
             <!-- Botón Login -->
-            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            <?= Html::submitButton('Logueo', ['submit class' => 'btn btn-primary', 'name' => 'login-button']) ?>
         </div>
         <div class="col-lg-offset-1 col-lg-11">
             <br>
@@ -44,4 +61,5 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 
     <?php ActiveForm::end(); ?>
+</center>
 </div>
