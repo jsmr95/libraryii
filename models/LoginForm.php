a75e20eb2fa9eafb7c5b2fe8b4a805b5d7767bc3
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-05-28 23:50:10 +0200

    Hago comprobacion en el login() de LoginForm

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -62,6 +62,16 @@ class LoginForm extends Model
     public function login()
     {
         if ($this->validate()) {
+            $user = $this->getUser();
+            if ($user->banned_at !== null) {
+                if ((new \DateTime()) > \DateTime::createFromFormat('Y-m-d H:i:s', $user->banned_at)) {
+                    $user->banned_at = null;
+                    $user->save(false);
+                } else {
+                    Yii::$app->session->setFlash('error', 'El usuario está baneado por 48h.');
+                    return false;
+                }
+            }
             return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
         }
         return false;
