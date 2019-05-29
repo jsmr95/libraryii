1906bd42101aaaa658718b2abc77886b1996d378
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-05-29 20:13:24 +0200

    Genero las urls pretties

--- views/libros/_libros.php
+++ views/libros/_libros.php
@@ -140,24 +140,13 @@ $this->registerJs($followJs);
                 </ul>
             </p>
             <div class="share-buttons">
-
-                <!-- Email -->
-                <a href="mailto:?Subject=Simple Share Buttons&amp;Body=I%20saw%20this%20and%20thought%20of%20you!%20 https://simplesharebuttons.com">
-                    <img src="https://simplesharebuttons.com/images/somacro/email.png" alt="Email" />
-                </a>
-
                 <!-- Facebook -->
-                <a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" target="_blank">
+                <a href="http://www.facebook.com/sharer.php?u=https://libraryii.herokuapp.com/index.php?r=libros%2Fview&id=<?=$model->id?>" target="_blank">
                     <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />
                 </a>
 
-                <!-- Google+ -->
-                <a href="https://plus.google.com/share?url=https://simplesharebuttons.com" target="_blank">
-                    <img src="https://simplesharebuttons.com/images/somacro/google.png" alt="Google" />
-                </a>
-
                 <!-- Twitter -->
-                <a href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
+                <a href="https://twitter.com/share?url=https://libraryii.herokuapp.com/index.php?r=libros%2Fview&id=<?=$model->id?>" target="_blank">
                     <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" />
                 </a>
 
