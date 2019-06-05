a5c0f1b469c458612bbdadd10084d171926f0407
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-06-05 18:32:58 +0200

    Modifico el site/index

--- views/site/index.php
+++ views/site/index.php
@@ -25,47 +25,27 @@ footer{
     display: none;
 }
 </style>
-    <?= Html::img(Yii::getAlias('@uploads').'/libreria.jpg', ['class' => 'portada']); ?>
+<?= Html::img(Yii::getAlias('@uploads').'/libreria.jpg', ['class' => 'portada']); ?>
 
-    <br><br>
-    <center>
-        <h1>¡Bienvenidos a Libraryii!</h1>
-    </center>
-    <br><br><br>
-    <div class="row">
-        <div class="col-md-8" style="font-size:28px">
-            <p>Libraryii es una red social orientada a todos los amantes de los
-                libros. En esta web podrás tener organizado el seguimiento de tus
-                libros. Clasificarlos en 'leidos', 'leyendo', 'por leer', marcar libros
-                como favoritos.</p><br>
-            <p>Si eres un amante de los libros y te gusta sumergite en un sin fin de
-        historias, estás en el lugar idóneo. Aquí podrás interactuar con otros usuarios,
-        comentar que te ha parecido un libro, hacer lybs, marcar como me favorito
-        libro e inclusive seguir a otros usuarios para ver sus interacciones.</p>
-        </div>
-        <div class="col-md-3 col-md-offset-1">
-            <center>
-                <h2 style="text-decoration: underline; ">Más valorado</h2>
-                <?php
-                $dataProvider = new ActiveDataProvider([
-                    'query' => Libros::find()->where(['id' =>$ultimoLanzamiento->id])
-                ]);
-                echo ListView::widget([
-                  'dataProvider' => $dataProvider,
-                  'summary' => '',
-                  'itemView' => '_ultimoLanzamientoMedia',
-              ]); ?>
-            </center>
-        </div>
+<br><br>
+<center>
+    <h1>¡Bienvenidos a Libraryii!</h1>
+</center>
+<br><br><br>
+<div class="row">
+    <div class="col-md-8" style="font-size:28px">
+        <p>Libraryii es una red social orientada a todos los amantes de los
+            libros. En esta web podrás tener organizado el seguimiento de tus
+            libros. Clasificarlos en 'leidos', 'leyendo', 'por leer', marcar libros
+            como favoritos.</p><br>
+        <p>Si eres un amante de los libros y te gusta sumergite en un sin fin de
+    historias, estás en el lugar idóneo. Aquí podrás interactuar con otros usuarios,
+    comentar que te ha parecido un libro, hacer lybs, marcar como me favorito
+    libro e inclusive seguir a otros usuarios para ver sus interacciones.</p>
     </div>
-    <div class="row">
-        <div class="col-md-8" style="font-size:28px">
-
-        </div>
-        <div class="col-md-3 col-md-offset-1">
-            <center>
-                <h2 style="text-decoration: underline; ">Último lanzamiento</h2>
-            </center>
+    <div class="col-md-3 col-md-offset-1">
+        <center>
+            <h2 style="text-decoration: underline; ">Más valorado</h2>
             <?php
             $dataProvider = new ActiveDataProvider([
                 'query' => Libros::find()->where(['id' =>$ultimoLanzamiento->id])
@@ -75,5 +55,18 @@ footer{
               'summary' => '',
               'itemView' => '_ultimoLanzamientoMedia',
           ]); ?>
-        </div>
+        </center>
+        <center>
+            <h2 style="text-decoration: underline; ">Último lanzamiento</h2>
+            <?php
+            $dataProvider = new ActiveDataProvider([
+                'query' => Libros::find()->where(['id' =>$ultimoLanzamiento->id])
+            ]);
+            echo ListView::widget([
+              'dataProvider' => $dataProvider,
+              'summary' => '',
+              'itemView' => '_ultimoLanzamientoMedia',
+            ]); ?>
+        </center>
     </div>
+</div>
