e7207606ae29a2b8d84799d40e319e4243168f6a
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-06-06 19:02:47 +0200

    Mejoro el responsive de libros/index

--- views/libros/_libros.php
+++ views/libros/_libros.php
@@ -54,6 +54,7 @@ margin-top: 10px;
 .dropdown {
     float: right;
     margin-bottom: 30px;
+    padding: 15px;
 }
 </style>
 
@@ -105,11 +106,11 @@ EOT;
 $this->registerJs($followJs);
 ?>
 
-<div class="row libro">
+<div class="row libro" itemscope itemtype="http://schema.org/Book">
     <!-- Fila para cada libro-->
-    <div class="col-md-12 col-xs-12 libro-cuerpo " itemscope itemtype="http://schema.org/Book">
+    <div class="col-md-12 col-xs-12 col-lg-12 libro-cuerpo " >
         <!-- Columna completa de cada libro-->
-        <div class="col-md-3 col-xs-3">
+        <div class="col-md-3 col-xs-3 col-lg-3">
             <!-- Columna de 3 para la imagen del libro-->
             <center>
                 <p itemprop="image">
@@ -123,84 +124,89 @@ $this->registerJs($followJs);
                 </p>
             </center>
         </div>
-        <div class="col-md-4 col-xs-4 libro-cabecera ">
-            <!-- Columna de 4 para la info de cada libro -->
-            <p itemprop="name">
-                <h2>
-                    <?= Html::a($model->titulo, [
-                    'libros/view',
-                    'id' => $model->id
-                    ]) ?>
-                    <!-- Botón para seguimiento -->
+        <div class="col-md-9 col-xs-9 col-lg-9 libro-cabecera ">
+            <div class="row">
+                <div class="col-md-9 col-xs-9 col-lg-9">
+                    <h2 itemprop="name">
+                        <?= Html::a($model->titulo, [
+                        'libros/view',
+                        'id' => $model->id
+                        ]) ?>
+                        <!-- Botón para seguimiento -->
+                        <?php
+                        $corazon = '';
+                        if (!Yii::$app->user->isGuest):
+                        $usuario = Usuarios::findOne(Yii::$app->user->id);
+                        $corazon = $usuario->consultaLibroSeguido($usuario->id, $model->id);
+                        ?>
+                        <!-- FAVORITO -->
+                        <button class="follow<?=$model->id?>" title="Marcar como favorito" data-libro="<?= $model->id ?>">
+                            <span id="estrella<?=$model->id?>" class='glyphicon glyphicon-star<?=$corazon?>' aria-hidden='true'></span>
+                        </button>
+                    </h2>
+                </div>
+                <div class="col-md-3 col-xs-3 col-lg-3">
                     <?php
-                    $corazon = '';
-                    if (!Yii::$app->user->isGuest):
-                    $usuario = Usuarios::findOne(Yii::$app->user->id);
-                    $corazon = $usuario->consultaLibroSeguido($usuario->id, $model->id);
-                    ?>
-                    <!-- FAVORITO -->
-                    <button class="follow<?=$model->id?>" title="Marcar como favorito" data-libro="<?= $model->id ?>">
-                        <span id="estrella<?=$model->id?>" class='glyphicon glyphicon-star<?=$corazon?>' aria-hidden='true'></span>
-                    </button>
-                </h2>
-
-                <ul>
-                    <li itemprop="author">Autor: <?= Html::a($model->autor->nombre, ['autores/view', 'id' => $model->autor->id]) ?></li>
-                    <li itemprop="copyrightYear">Año: <?= $model->anyo ?></li>
-                    <li itemprop="genre">Género: <?= $model->genero->genero ?></li>
-                    <li itemprop="isbn">ISBN: <?= $model->isbn ?></li>
-                    <li title="Enlace a compra"> <?= Html::a('Compra', $model->url_compra) ?>
-                    </li>
-                </ul>
-            </p>
-            <div class="share-buttons">
-                <!-- Facebook -->
-                <a href="http://www.facebook.com/sharer.php?u=https://libraryii.herokuapp.com/index.php?r=libros%2Findex" target="_blank" title="Compartir en Facebook">
-                    <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />
-                </a>
-
-                <!-- Twitter -->
-                <a href="https://twitter.com/share?url=https://libraryii.herokuapp.com/index.php?r=libros%2Findex" target="_blank" title="Compartir en Twitter">
-                    <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" />
-                </a>
-
-            </div>
-        </div>
-        <div class="col-md-5 col-xs-5 libro-texto " itemprop="description">
-            <div class="row">
-                <?php
-                $seguimientoStr = '...';
-                $seguimiento = Seguimientos::find()
-                ->where(['usuario_id' => $usuarioId, 'libro_id' => $id])
-                ->one();
-                if ($seguimiento) {
-                    if ($seguimiento->estado_id == 1) {
-                        $seguimientoStr = "Leído <span class='glyphicon glyphicon-chevron-down'></span>";
-                    } else if ($seguimiento->estado_id == 2) {
-                        $seguimientoStr = "Leyendo  <span class='glyphicon glyphicon-chevron-down'></span>";
-                    } else {
-                        $seguimientoStr = "Me gustaría leerlo  <span class='glyphicon glyphicon-chevron-down'></span>";
+                    $seguimientoStr = '...';
+                    $seguimiento = Seguimientos::find()
+                    ->where(['usuario_id' => $usuarioId, 'libro_id' => $id])
+                    ->one();
+                    if ($seguimiento) {
+                        if ($seguimiento->estado_id == 1) {
+                            $seguimientoStr = "Leído <span class='glyphicon glyphicon-chevron-down'></span>";
+                        } else if ($seguimiento->estado_id == 2) {
+                            $seguimientoStr = "Leyendo  <span class='glyphicon glyphicon-chevron-down'></span>";
+                        } else {
+                            $seguimientoStr = "Me gustaría leerlo  <span class='glyphicon glyphicon-chevron-down'></span>";
+                        }
                     }
-                }
-                ?>
+                    ?>
 
-                <span class="dropdown" title="Seguimiento">
-                  <button style="margin-left:20px" class="btn btn-default dropdown-toggle" id="dropdownMenu<?=$id?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    <?= $seguimientoStr ?>
-                  </button>
-                  <ul class="dropdown-menu dropdown<?=$id?>" aria-labelledby="dropdownMenu<?=$id?>" data-seguimiento="<?= $id ?>">
-                    <li data-id='1'><a>Leído</a></li>
-                    <li data-id='2'><a>Leyendo</a></li>
-                    <li data-id='3'><a>Me gustaría leerlo</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li data-id='4'><a>Limpiar seguimiento</a></li>
-                  </ul>
-                </span>
-                <?php endif; ?>
+                    <span class="dropdown" title="Seguimiento">
+                      <button style="margin-left:20px" class="btn btn-default dropdown-toggle" id="dropdownMenu<?=$id?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <?= $seguimientoStr ?>
+                      </button>
+                      <ul class="dropdown-menu dropdown<?=$id?>" aria-labelledby="dropdownMenu<?=$id?>" data-seguimiento="<?= $id ?>">
+                        <li data-id='1'><a>Leído</a></li>
+                        <li data-id='2'><a>Leyendo</a></li>
+                        <li data-id='3'><a>Me gustaría leerlo</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li data-id='4'><a>Limpiar seguimiento</a></li>
+                      </ul>
+                    </span>
+                    <?php endif; ?>
+                </div>
             </div>
             <div class="row">
-                <!-- Columna de 5 para la sinopsis-->
-                <?= $model->sinopsis  ?>
+                <div class="col-lg-4 col-md-4 col-xs-4">
+                    <ul>
+                        <li itemprop="author">Autor: <?= Html::a($model->autor->nombre, ['autores/view', 'id' => $model->autor->id]) ?></li>
+                        <li itemprop="copyrightYear">Año: <?= $model->anyo ?></li>
+                        <li itemprop="genre">Género: <?= $model->genero->genero ?></li>
+                        <li itemprop="isbn">ISBN: <?= $model->isbn ?></li>
+                        <li title="Enlace a compra"> <?= Html::a('Compra', $model->url_compra) ?>
+                        </li>
+                    </ul>
+                    <div class="share-buttons">
+                        <!-- Facebook -->
+                        <a href="http://www.facebook.com/sharer.php?u=https://libraryii.herokuapp.com/index.php?r=libros%2Findex" target="_blank" title="Compartir en Facebook">
+                            <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />
+                        </a>
+
+                        <!-- Twitter -->
+                        <a href="https://twitter.com/share?url=https://libraryii.herokuapp.com/index.php?r=libros%2Findex" target="_blank" title="Compartir en Twitter">
+                            <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" />
+                        </a>
+
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-8 col-xs-8">
+                    <div itemprop="description">
+                        <!-- Columna de 5 para la sinopsis-->
+                        <?= $model->sinopsis  ?>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
